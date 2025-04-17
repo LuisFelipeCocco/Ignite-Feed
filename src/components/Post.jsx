6ca68f1cfc915798a20d2d798f5255a7e8feea24
LuisFelipeCocco <luisfelipe.cocco@hotmail.com>
2025-04-17 17:53:06 -0300

    primeiro commit

---
+++ src/components/Post.jsx
@@ -0,0 +1,73 @@
+import { format , formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
+
+import styles from "./Post.module.css";
+
+import PropTypes from 'prop-types';
+
+export function Post({ author,publishedAt, content }) {
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    })
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    })
+
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl} />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
+
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        {content.map((line, index) => {
+            if (line.type === 'paragraph') {
+                return <p key={index}>{line.content}</p>;
+            } else if (line.type === 'link') {
+                return <p key={index}><a href="">{line.content}</a></p>;
+            }
+        })}
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+    </article>
+  );
+}
+Post.propTypes = {
+    author: PropTypes.shape({
+      avatarUrl: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      role: PropTypes.string.isRequired,
+    }).isRequired,
+    publishedAt: PropTypes.instanceOf(Date).isRequired,
+    content: PropTypes.array,
+  };
